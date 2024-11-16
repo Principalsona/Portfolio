915efe01ba0da66db225db3b831e09491c0746b3
KIXSZH <kishorekumar150404@gmail.com>
2024-11-16 23:47:47 +0530

    worldmap add

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -14,6 +14,7 @@ import Projects from "@/components/Projects";
 import Socials from "@/components/Socials";
 import Splash from "@/components/Splash";
 import Form from "@/components/Form";
+import Map from "@/components/Datamap";
 import Technical from "@/components/Technical";
 import styles from "./Home.module.css";
 
@@ -45,6 +46,7 @@ const App = ({ initialTheme }: { initialTheme: string }) => {
         )}
         {!showForm && <About />}
         {!showForm && <Experiences />}
+        {!showForm && <Map />}
         {!showForm && <Projects />}
         {!showForm && <Technical />}
         {!showForm && <Education />}
